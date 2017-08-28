897d577096ea4ad2761e748caa3f93ae61c5ca57
preejol <preetijolapra11@gmail.com>
2017-08-28 15:14:12 +0530

    Update BluetoothSerialService.java

--- src/android/com/megster/cordova/BluetoothSerialService.java
+++ src/android/com/megster/cordova/BluetoothSerialService.java
@@ -216,6 +216,23 @@ public class BluetoothSerialService {
         // Perform the write unsynchronized
         r.write(out);
     }
+	
+	/**
+     * Write to the ConnectedThread in an unsynchronized manner
+     * @param out The bytes to write
+     * @see ConnectedThread#write(byte[])
+     */
+    public void barcode(String out) {
+        // Create temporary object
+        ConnectedThread r;
+        // Synchronize a copy of the ConnectedThread
+        synchronized (this) {
+            if (mState != STATE_CONNECTED) return;
+            r = mConnectedThread;
+        }
+        // Perform the write unsynchronized
+        r.barcode(out);
+    }
 
     /**
      * Indicate that the connection attempt failed and notify the UI Activity.
